fe2b3c01f75746cd17e97542509ebb7fad3cdbfe
Arturo <arturoazuara92@aragon.unam.com>
2024-10-31 16:37:43 -0600

    web1node

---
+++ web1node/holamundo/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Web 1 ico FES Aragon' });
+});
+
+router.get('/ico', function(req,res,next){
+  res.render('formulario', {titulo: 'Ejemplo'} );
+});
+
+module.exports = router;
