2a62d0b413d2abbb729f7055e30dbdc8f04972f6
Arturo <arturoazuara92@aragon.unam.com>
2024-11-07 16:48:27 -0600

    mysql y cosas

---
+++ proyectofinal/config/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user: 'user1',
+    password: 'patito123',
+    database: 'librosdb'
+});
+
+connection.connect(error => {
+    if (error){
+        console.error('Error de conexion' , error);
+        return;
+    }else{
+        console.log('Conexión establecida!!!');
+    }
+});
+
+
+module.exports = connection;
+
--- proyectofinal/routes/index.js
+++ proyectofinal/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../config/db.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/libros',(req, res, next) => {
+  const sql = 'SELECT * FROM libros';
+
 });
 
 module.exports = router;
